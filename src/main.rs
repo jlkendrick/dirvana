685e31b37825a2a42d365cb73312e9770630d23d
James <kendrickj5@yahoo.com>
2024-12-14 03:45:44 -0600

    Update argument in collect_directories

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use utils::file_ops;
 
 fn main() {
 	let start_dir = std::path::PathBuf::from("..");
-	let directories = file_ops::collect_directories(start_dir);
+	let directories = file_ops::collect_directories(&start_dir);
 
 	for directory in directories {
 		println!("{}", directory);
--- src/utils/file_ops.rs
+++ src/utils/file_ops.rs
@@ -3,12 +3,13 @@ use std::path::Path;
 use std::fs;
 
 // Collects all the directories starting from a given root
-pub fn collect_directories<P: AsRef<Path>>(root: P) -> Vec<String> {
+pub fn collect_directories(root: &Path) -> Vec<String> {
 	let mut directories = Vec::new();
 	let mut queue = VecDeque::new();
 	let mut visited = HashSet::new();
 
-	queue.push_back(root.as_ref().to_path_buf());
+	queue.push_back(root.to_path_buf());
+	visited.insert(root.to_path_buf());
 
 	// While there are still directories to visit
 	while let Some(current_dir) = queue.pop_front() {
