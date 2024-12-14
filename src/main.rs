480d699c46214824a31b00318473d53841b618d5
James <kendrickj5@yahoo.com>
2024-12-14 03:38:52 -0600

    Add algorithm to collect directory names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod utils;
+
+use utils::file_ops;
+
 fn main() {
-    println!("Hello, world!");
+	let start_dir = std::path::PathBuf::from("..");
+	let directories = file_ops::collect_directories(start_dir);
+
+	for directory in directories {
+		println!("{}", directory);
+	}
 }
---
+++ src/utils/file_ops.rs
@@ -0,0 +1,40 @@
+use std::collections::{HashSet, VecDeque};
+use std::path::Path;
+use std::fs;
+
+// Collects all the directories starting from a given root
+pub fn collect_directories<P: AsRef<Path>>(root: P) -> Vec<String> {
+	let mut directories = Vec::new();
+	let mut queue = VecDeque::new();
+	let mut visited = HashSet::new();
+
+	queue.push_back(root.as_ref().to_path_buf());
+
+	// While there are still directories to visit
+	while let Some(current_dir) = queue.pop_front() {
+
+		// If we have already visited this directory, skip it
+		if !visited.insert(current_dir.clone()) {
+			continue;
+		}
+
+		// Get an iterator over the entries in the current directory
+		if let Ok(entries) = fs::read_dir(&current_dir) {
+			for entry in entries.flatten() {
+				let path = entry.path();
+
+				println!("Path: {:?}", path);
+
+				// If the entry is a directory, add it to the queue
+				if path.is_dir() {
+					if let Some(directory) = path.to_str() {
+						directories.push(directory.to_string());
+					}
+					queue.push_back(path);
+				}
+			}
+		}
+	}
+
+	directories
+}
