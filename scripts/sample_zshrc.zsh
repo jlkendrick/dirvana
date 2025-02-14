0eacbc5c69c39b24a7fc7cfec8d23a4f828315ca
James <kendrickj5@yahoo.com>
2025-02-14 17:26:46 -0600

    Refactor completion settings in _dv script and move to sample_zshrc.zsh

--- scripts/sample_zshrc.zsh
+++ scripts/sample_zshrc.zsh
@@ -1,4 +1,12 @@
 fpath=(~/.zsh/completions $fpath)
+
+zstyle ':completion:*' list-grouped yes
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list '' 'r:|=*'
+
+setopt menucomplete
+setopt autolist
+
 autoload -Uz compinit && compinit
 
 # The other content of your existing .zshrc file #
