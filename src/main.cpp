c6109af369052d6d684d8b539a282c08c777b889
James <kendrickj5@yahoo.com>
2025-02-26 04:37:22 -0600

    Add notes for future development.

--- src/main.cpp
+++ src/main.cpp
@@ -31,28 +31,31 @@ vector<string> query(const string& partial) {
 	return completer.get_all_matches(partial);
 }
 
-// Arguments that can be passed to the program
-// 'dv build' - Build the DirectoryCompleter and save it to a file
-// 'dv update <path>' - Update the DirectoryCompleter by promoting the given path (used internally after 'dv'-ing into a directory)
-// 'dv <partial>' - Query the DirectoryCompleter for matches to the partial path
-// 'dv -- <partial>' - Used to bypass potential name conflicts with the above commands, functions the same as 'query'
+
 int main(int argc, char* argv[]) {
-	if (argc < 2)
-		// We expect at least 2 arguments, the name of the program and the command
+
+
+
+	if (argc < 3) {
+		// We expect at least 2 arguments, the program name, the flag, and some command or path
+		cout << "Usage: dv <command> [args]" << endl;
 		return 0;
+	}
 
 	string command = argv[1];
 	if (command == "rebuild") {
 		// Rebuild the DirectoryCompleter from scratch and save it to a file, resetting order
 		rebuild();
 		return 0;
+	}
 
-	} else if (command == "refresh") {
+	if (command == "refresh") {
 		// Rebuild the DirectoryCompleter while preserving the existing data and save it to a file (way to account for added/removed directories)
 		refresh();
 		return 0;
+	}
 
-	} else if (command == "update") {
+	if (command == "update") {
 		// First, we need to check if we have an argument for the path
 		if (argc < 3)
 			// We expect at least 2 arguments, the command and the path
