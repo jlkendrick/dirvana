12460076a3c7f8320747bcfbfac2cb8be4ada50a
James <kendrickj5@yahoo.com>
2025-05-01 18:47:25 -0500

    Create indexes on table

--- include/Database.h
+++ include/Database.h
@@ -21,13 +21,25 @@ private:
 	const Config& config;
 
 	void create_table() {
-		db << "CREATE TABLE IF NOT EXISTS paths ("
-			  "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-			  "path TEXT NOT NULL, "
-			  "dir_name TEXT NOT NULL, "
-			  "last_accessed INTEGER NOT NULL, "
-			  "access_count INTEGER NOT NULL DEFAULT 0"
-			  ");";
+		try {
+			db << "BEGIN TRANSACTION;";
+
+			db << "CREATE TABLE IF NOT EXISTS paths ("
+				"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+				"path TEXT NOT NULL, "
+				"dir_name TEXT NOT NULL, "
+				"last_accessed INTEGER NOT NULL, "
+				"access_count INTEGER NOT NULL DEFAULT 0"
+				");";
+			db << "CREATE UNIQUE INDEX IF NOT EXISTS idx_path ON paths (path);";
+			db << "CREATE INDEX IF NOT EXISTS idx_paths_dir_recency ON paths (dir_name, last_accessed DESC);";
+			db << "CREATE INDEX IF NOT EXISTS idx_paths_dir_freq ON paths (dir_name, access_count DESC);";
+
+			db << "COMMIT;";
+		} catch (const sqlite::sqlite_exception& e) {
+			db << "ROLLBACK;";
+			std::cerr << "Error creating table: " << e.what() << std::endl;
+		}
 	};
 	void drop_table() { db << "DROP TABLE IF EXISTS paths;"; };
 
